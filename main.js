662c5b1714523377fb8bc94ffaa12e0c8f3496d2
jeffsusanto <susantojeffy@gmail.com>
2022-07-21 22:57:31 +0800

    flexible box sizes and ability to set grid size

--- main.js
+++ main.js
@@ -1,11 +1,27 @@
 const container = document.querySelector('.container')
+const button = document.querySelector('.set')
+button.addEventListener('click', () => {
+    size = prompt('enter any number')
+    
+    newSketch(size)  
+})
 
-const squares = function() {
-    for(let i=0; i<16*16; i++){
+const newSketch = function (size) {
+    for (let i = 0; i < size * size; i++) {
         const pixel = document.createElement('div')
-        pixel.classList.add('pixel')
+        pixel.classList.toggle(`pixel`)
+        pixel.style.width = 500/size + 'px'
+        pixel.style.height = 500/size + 'px'
+        
         container.appendChild(pixel)
+    
     }
+
+    const divPixel = document.querySelectorAll('.pixel');
+    divPixel.forEach(item => item.addEventListener("mouseover",() => {
+      item.style.backgroundColor = 'black'
+      item.style.borderColor = 'black'
+    }))
 }
 
-squares()
+
